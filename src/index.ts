02edce78f6fbf82de965b2f51f00579e780c7e03
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-05-07 15:08:09 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import "express-async-errors";
-import express, { Request, Response } from "express";
-import routes from "./routes/index.js";
-import cors from "cors";
-import "dotenv/config.js";
-import errorMiddleware from "./middlewares/error.js";
-import boot from "./services/boot.js";
+import 'express-async-errors';
+import express, { Request, Response } from 'express';
+import routes from './routes/index.js';
+import cors from 'cors';
+import 'dotenv/config.js';
+import errorMiddleware from './middlewares/error.js';
+import boot from './services/boot.js';
 
 const app = express();
 
-app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+app.use(cors({ origin: '*', methods: 'GET,POST,PUT,DELETE,OPTIONS' }));
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   return res.status(200).json({
     success: true,
-    message: "Welcome to backend!",
+    message: 'Welcome to backend!',
   });
 });
 
-app.use("/api", routes);
+app.use('/api', routes);
 app.use(errorMiddleware);
 
 boot().then(() => {
   app.listen(process.env.PORT || 5000, () => {
-    console.log("log: connected");
+    console.log('log: connected');
   });
 });
--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from "express";
-import { sendResponse } from "../helpers.js";
+import { NextFunction, Request, Response } from 'express';
+import { sendResponse } from '../helpers.js';
 
 const errorMiddleware = (
   err: Error,
